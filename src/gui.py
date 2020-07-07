34ba5fb4382dc9e607c856dd744abdbe8763cc71
Kyungwon Chun <ruddyscent@gmail.com>
2020-07-08 00:23:28 +0900

    Update gui.py

--- src/gui.py
+++ src/gui.py
@@ -42,52 +42,51 @@ class pyqt_ipcam(QWidget):
         self.setLayout(self.grid)
         self.show()        
         
-    # image read   
+    # Read image   
     def run(self):        
         self.get_target()        
-        x = [int(i/scale) for i in self.x]
-        y = [int(i/scale) for i in self.y]
+        x = [i // scale for i in self.x]
+        y = [i // scale for i in self.y]
         while running:
             img = cv2.imread(f'images/2020070313_{self.camnumber}.png', cv2.IMREAD_COLOR)            
-            h, w, c= img.shape
-            dim = (int(w/scale), int(h/scale))
+            h, w, c = img.shape
+            dim = int(w / scale), int(h / scale)
             self.img = img
-            img = cv2.resize(img, None, fx = 1 / scale, fy = 1 / scale, interpolation = cv2.INTER_AREA)
-            h, w, c= img.shape
+            img = cv2.resize(img, None, fx=1/scale, fy=1/scale, interpolation=cv2.INTER_AREA)
+            h, w, c = img.shape
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             for i in range(len(x)):
                 cv2.rectangle(img, (x[i] - 5, y[i] - 5), (x[i] + 5, y[i] + 5), (0, 255, 0), 1)
                 pixel = str(self.d[i]) + "km"
                 cv2.putText(img, pixel, (x[i] + 3, y[i] - 10), cv2.FONT_HERSHEY_COMPLEX, 0.3, (255, 0, 0))                        
             
-            # bottom black rectangle
+            # Dark background for the textual contents on the bottom
             subimg = img.copy()
-            cv2.rectangle(subimg, (0, h-100), (w,   h), (0, 0, 0), -1)
+            cv2.rectangle(subimg, (0, h - 100), (w, h), (0, 0, 0), -1)
             img = cv2.addWeighted(img, 0.5, subimg, 0.5, 1)
             
-            # bottom content
-            # content text
-            text_position = (50, h - 40) 
+            # Header for measured values
+            text_position = (50, h - 40)
             cv2.putText(img, 'Visibility', (text_position[0], text_position[1] - 10),  
                         cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255))
-            cv2.putText(img, 'PM10', (text_position[0]+180,   text_position[1] - 10),  
+            cv2.putText(img, 'PM10', (text_position[0] + 180,   text_position[1] - 10),  
                         cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255))
-            cv2.putText(img, 'Temp.', (text_position[0]+330,   text_position[1] - 10),  
+            cv2.putText(img, 'Temp.', (text_position[0] + 330,   text_position[1] - 10),  
                         cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255))
-            cv2.putText(img, 'Humidity', (text_position[0]+460, text_position[1] - 10),  
+            cv2.putText(img, 'Humidity', (text_position[0] + 460, text_position[1] - 10),  
                         cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255))
-            cv2.putText(img, 'Atm. Pres.', (text_position[0]+610, text_position[1] - 10),
+            cv2.putText(img, 'Atm. Pres.', (text_position[0] + 610, text_position[1] - 10),
                         cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255))
             
             self.bottom_value()
             
-            # value text
-            value_position = (50, h - 15)            
+            # Values of the content
+            value_position = (50, h - 15)
             cv2.putText(img, f'{self.vi} km', (value_position[0] + 5, value_position[1] - 10),
                         cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255))
-            cv2.putText(img, f'{self.md} ug/m3', (value_position[0]+160, value_position[1] - 10),  
+            cv2.putText(img, f'{self.md} ug/m3', (value_position[0] + 160, value_position[1] - 10),  
                         cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255))
-            cv2.putText(img, f'{self.tm} C', (value_position[0] + 335, value_position[1] - 10),
+            cv2.putText(img, f'{self.tm} oC', (value_position[0] + 335, value_position[1] - 10),
                         cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255))
             cv2.putText(img, f'{self.hm} %', (value_position[0] + 480, value_position[1] - 10),
                         cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255))
@@ -99,10 +98,10 @@ class pyqt_ipcam(QWidget):
                         cv2.FONT_HERSHEY_COMPLEX, 0.4, (255, 255, 255))
             qImg = QtGui.QImage(img.data, w, h, w * c, QtGui.QImage.Format_RGB888)
             pixmap = QtGui.QPixmap.fromImage(qImg)
-            self.label1.setPixmap(pixmap)    
-        print('Thread end')    
+            self.label1.setPixmap(pixmap)
+        print('Thread end')
         
-    #video run    
+    # Start video stream
     def start(self, index: int):
         global running
         running = True
@@ -110,17 +109,17 @@ class pyqt_ipcam(QWidget):
             self.camnumber = 'nomal'
             th1 = threading.Thread(target=self.run)
             th1.start()
-            print("nomal Camera started..")
+            print("Nomal camera started...")
         elif index == 2:
             self.camnumber = 'ir'
             th1 = threading.Thread(target=self.run)
             th1.start()
-            print("ir Camera started..")
+            print("IR camera started...")
             
     def stop(self):
         global running
         running = False        
-        print("Stop..")
+        print("Stop...")
         
     def get_target(self):
         df = pd.read_csv(f"data/{self.camnumber}.csv")
@@ -131,21 +130,21 @@ class pyqt_ipcam(QWidget):
     def img_capture(self, filename: str, img: np.ndarray):        
         path = str(os.getcwd())
         cmd = f"raspistill -hf -vf -e png -o {path}/images/{filename}.png"
-        print('Please wait')
+        print("Please wait")
         os.system(cmd)
-        print("Image Save")
+        print("Save image")
         
     def bottom_value(self):        
         a = [0.1, 0.12, 0.21, 0.28, 0.91, 0.93, 0.33, 1.3, 2.0,
              3.7, 5.4, 5.0, 6.5, 1.7]        
-        i = random.randint(0, len(a)-1)
+        i = random.randint(0, len(a) - 1)
         self.vi = str(a[i])        
         self.md = str(random.randint(20, 60))
         self.tm = str(random.randint(28, 33))
         self.hm = str(random.randint(40, 70))
         self.br = str(random.randint(990, 1000))
         
-    def keyPressEvent(self,   e: QtGui.QKeyEvent):
+    def keyPressEvent(self, e: QtGui.QKeyEvent):
         print(type(e))
         # nomal image read
         if e.key() == Qt.Key_Q:            
@@ -154,26 +153,23 @@ class pyqt_ipcam(QWidget):
             time.sleep(1)            
             self.start(1)
             
-        # ir image read
+        # Read IR image
         elif e.key() == Qt.Key_W:
             self.stop()
             time.sleep(1)            
             self.start(2)
-        # program end
+        # Quit program
         elif e.key() == Qt.Key_G:
             self.stop()
             self.close()
             
 def main():
     app = QApplication(sys.argv)
-    ex=pyqt_ipcam()
-    #Default nomal image read
+    ex = pyqt_ipcam()
+    # Default behavior is to read nomal image
     ex.start(1)
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
     main()
-
-
-
-
+    
