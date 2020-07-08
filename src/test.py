623641d5f503dfc82c19da4f53b6355c39781a1a
Chae Seong Min <fire5450@naver.com>
2020-07-08 17:04:18 +0900

    'test파일삭제'

--- src/test.py
+++
@@ -1,9 +0,0 @@
-import glob
-import os
-import cv2
-print(os.getcwd())
-path = "images/2020070810/"
-num = "v2"
-
-img = cv2.imread(glob.glob(f"{path}*_{num}.png")[0], cv2.IMREAD_COLOR) 
-print(type(img))
