e18282306fd0a3bd262f789e9aa2886979696884
Oh-JongJin <5jx2oh@gmail.com>
2020-08-25 09:15:52 +0900

    Add code to access InfluxDB and insert sensor values

--- src/aws_test.py
+++ src/aws_test.py
@@ -13,6 +13,8 @@ import binascii
 import struct
 import sys
 
+from influxdb import InfluxDBClient
+
 
 def serial_test():
     """The connected weather sensor outputs six values.
@@ -21,31 +23,46 @@ def serial_test():
     """
     global wind_speed, i
     try:
-        read_aws = b'\x01\x03\x00\x00\x00\x29\x84\x14'
+        write_aws = b'\x01\x03\x00\x00\x00\x29\x84\x14'
         ser = serial.Serial(port="COM9", baudrate=9600, parity=serial.PARITY_EVEN,
                             stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS,
                             timeout=1)
         if ser.readable():
-            ser.write(read_aws)
+            ser.write(write_aws)
             res = ser.readline()
-            print(f"\nWind direction: {int(binascii.hexlify(res[5:7]), 16)}°")  # Wind direction
+            wind_direction = int(binascii.hexlify(res[5:7]), 16)
+            print(f"\nWind direction: {wind_direction}°")  # Wind direction
 
-            wind_speed_hex = f"{hex(res[8]) + ' ' + hex(res[7]) + ' ' + hex(res[10]) + ' ' + hex(res[9])}"
-            temp_hex = f"{hex(res[12]) + ' ' + hex(res[11]) + ' ' + hex(res[14]) + ' ' + hex(res[13])}"
-            humid_hex = f"{hex(res[16]) + ' ' + hex(res[15]) + ' ' + hex(res[18]) + ' ' + hex(res[17])}"
-            pressure_hex = f"{hex(res[20]) + ' ' + hex(res[19]) + ' ' + hex(res[22]) + ' ' + hex(res[21])}"
-            pm25_hex = f"{hex(res[54]) + ' ' + hex(res[53]) + ' ' + hex(res[56]) + ' ' + hex(res[55])}"
+            # print("Speed section: ", hex(res[7]), hex(res[8]), hex(res[9]), hex(res[10]))           # Wind speed section
+            # print("Temp section: ", hex(res[11]), hex(res[12]), hex(res[13]), hex(res[14]))          # Temp section
+            # print("Humid section: ", hex(res[15]), hex(res[16]), hex(res[17]), hex(res[18]))          # Humid section
+            # print("Atmospheric Pressure: ", hex(res[19]), hex(res[20]), hex(res[21]), hex(res[22]))    # Press section
+            # print("PM2.5 : ", hex(res[53]), hex(res[54]), hex(res[55]), hex(res[56]))                   # PM2.5 section
+
+            wind_speed_hex = f"{hex(res[8]) + ' ' + hex(res[7]) + ' ' + hex(res[10]) + ' ' + hex(res[9])}".replace(
+                "0x0", "0x00")
+            temp_hex = f"{hex(res[12]) + ' ' + hex(res[11]) + ' ' + hex(res[14]) + ' ' + hex(res[13])}".replace(
+                "0x0 ", "0x00 ")
+            humid_hex = f"{hex(res[16]) + ' ' + hex(res[15]) + ' ' + hex(res[18]) + ' ' + hex(res[17])}".replace(
+                "0x0 ", "0x00 ")
+            pressure_hex = f"{hex(res[20]) + ' ' + hex(res[19]) + ' ' + hex(res[22]) + ' ' + hex(res[21])}".replace(
+                "0x0 ", "0x00 ")
+            pm25_hex = f"{hex(res[54]) + ' ' + hex(res[53]) + ' ' + hex(res[56]) + ' ' + hex(res[55])}".replace(
+                "0x0 ", "0x00 ")
 
             wind_speed_non = wind_speed_hex.replace("0x", '')
             temp_non = temp_hex.replace("0x", '')
             humid_non = humid_hex.replace("0x", '')
             pressure_non = pressure_hex.replace("0x", '')
             pm25_non = pm25_hex.replace("0x", '')
+            # print(f"\nWind Speed: {wind_speed_non}\nTemp: {temp_non}\nHumid: {humid_non}\nPressure: {pressure_non}"
+            #       f"\nPM2.5: {pm25_non}")
 
-            if wind_speed_non == '0 0 0 0':
+            # data = pressure_non
+            if wind_speed_non == '00 00 00 00':
                 wind_speed = (1,)
                 lst_ws = list(wind_speed)
-                lst_ws[0] = 0
+                lst_ws[0] = 0.01
                 wind_speed = tuple(lst_ws)
             else:
                 wind_speed = struct.unpack('<f', binascii.unhexlify(wind_speed_non.replace(' ', '')))
@@ -58,7 +75,20 @@ def serial_test():
                   f"\nAtmospheric Pressure: {round(pressure[0], 2)} hPa \nHumidity: {round(humid[0], 2)} %"
                   f"\nPM2.5: {round(pm25[0], 2)} ug/m3")
 
-            time.sleep(1)
+            client = InfluxDBClient("192.168.85.129", 8086)
+            client.switch_database("AWS")
+            points = [
+                {"measurement": "test1",
+                 "tags": {"name": "OJJ"},
+                 "fields": {"wind_direction": float(int(binascii.hexlify(res[5:7]), 16)),
+                            "wind_speed": float(wind_speed[0]), "temp": round(temp[0], 2),
+                            "humid": round(humid[0], 2), "pressure": round(pressure[0], 2),
+                            "pm25": round(pm25[0], 2)},
+                 "time": time.time_ns()}
+            ]
+            client.write_points(points=points, protocol="json")
+
+            time.sleep(3)
         ser.close()
 
     except Exception as e:
