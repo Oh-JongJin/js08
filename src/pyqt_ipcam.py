4f280fa27e753c90f2618fb4ac8dae2da211310e
Chae Seong Min <fire5450@naver.com>
2020-09-01 16:45:09 +0900

    파노라마 카메라 연동 및 타겟 이미지 저장

--- src/pyqt_ipcam.py
+++ src/pyqt_ipcam.py
@@ -7,9 +7,10 @@ Typical usage example:
     python pyqt_ipcam.py
 Keyboard key:
     
-    1 : Read Hanhwa camera 
+    1 : Read Hanhwa panorama camera
     2 : Read Commax camera
     3 : Read Image
+    4 : Read Hanhwa camera 
 Mouse event:
     
     Left click : Add target
@@ -27,19 +28,16 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QGridLayout, QWidget, QLabel, QApplication, QLineEdit, QInputDialog
 
 # UI를 정의하는 클래스
-# 키입력으로 바꿔서 적용하려면 클래스를 만들어서 해야함.
+# 키, 마우스 입력을 받으려면 QWidget 클래스를 상속 받아야 한다.
 class pyqt_ipcam(QWidget):
 
     def __init__(self):
-        super().__init__()
-        # label의 크기에 따라 Widget의 크기를 변경한다. 위젯은 변하지만 영상의 크기는 고정되어 있기때문에 따로 
-        # 설정해줘야 한다.
-        self.grid = QGridLayout()
+        super().__init__()        
+        self.grid = QGridLayout()        
         self.label = QLabel()        
-        # self.setGeometry(50, 50, 1820, 980) 
-        self.showFullScreen()
+        self.setGeometry(0, 150, 1920, 800)       
         self.grid.addWidget(self.label, 0, 0)
-        self.grid.setContentsMargins(0, 0, 0, 0)     
+        self.grid.setContentsMargins(0, 0, 0, 0)   
         self.setLayout(self.grid)
         self.show()
         
@@ -50,33 +48,19 @@ class pyqt_ipcam(QWidget):
         else:            
             self.cap = cv2.VideoCapture(self.ADD)
             width = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-            height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-            print("video size", width,", ", height)
-            # self.label.resize(width, height)
-
-        # 초기 사이즈 값을 저장한다.
-        prewidth = width
-        preheight = height
+            height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)            
 
         while running:
-            #마우스 트래킹 감지 False이면 클릭시 이동을 감지
-            self.setMouseTracking(False)
             
+            # 시간 저장
+            epoch = time.strftime("%Y%m%d%H%M%S", time.localtime(time.time()))
+
+            #마우스 트래킹 감지 False이면 클릭시 이동을 감지
+            self.setMouseTracking(False)            
             # camname이 이미지가 아닐 때 cap으로 읽기
             if not self.camname == "img":
                 ret, img = self.cap.read()
-                # TODO(성민): 윈도 크기를 작게 조정하는 기능이 작동하지 않습니다.            
-                # resize의 fx는 비율로 크기를 조절한다.
-                # 레이블의 크기에 따라 영상 크기도 같이 변한다.
-                # if prewidth <= self.label.width() or preheight <= self.label.height():
-                #     img = cv2.resize(img, dsize=(0, 0), fx=mowidth, fy=moheight, 
-                #                         interpolation=cv2.INTER_LINEAR)
-                if prewidth > self.label.width() or preheight > self.label.height():
-                    img = cv2.resize(img, dsize=(0, 0), 
-                            fx=self.label.width()/width, 
-                            fy=self.label.height()/height, 
-                            interpolation=cv2.INTER_AREA)        
-
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             # camname이 이미지가 일 때 imread 메서드로 읽기        
             else:
                 img = cv2.imread("test.png", cv2.IMREAD_COLOR)
@@ -89,32 +73,43 @@ class pyqt_ipcam(QWidget):
                 if len(self.x) >= 0:
                     try:
                         for i in range(len(self.x)):
-                            upper_left = self.x[i] - 5, self.y[i] - 5
-                            lower_right = self.x[i] + 5, self.y[i] + 5
-                            cv2.rectangle(img, upper_left, lower_right, (0, 255, 0), 1)
+                            if epoch[-3:] == "500" or epoch[-3:] == "000":
+                                try:
+                                    if not(os.path.isdir(f"target/target{i+1}")):
+                                        os.makedirs(os.path.join(f"target/target{i+1}"))
+
+                                    if not(os.path.isfile(f"target/target{i+1}/{epoch}.png")):
+                                        # 모델에 넣을 이미지 추출
+                                        cropimg = img[self.y[i] - 112 : self.y[i] + 112 , self.x[i] - 112 : self.x[i] + 112]
+                                        # cv로 저장할 때는 bgr 순서로 되어 있기 때문에 rgb로 바꿔줌.
+                                        b, g, r = cv2.split(cropimg)
+                                        # 타겟의 각 폴더에 크롭한 이미지 저장
+                                        cv2.imwrite(f"target/target{i+1}/{epoch}.png", cv2.merge([r, g, b]))
+                                except OSError as e:
+                                    if e.errno != errno.EEXIST:    
+                                        pass
+                            else:
+                                pass
+                            
+                            # UI 이미지에 사각형으로 표시
+                            upper_left = self.x[i] - 50, self.y[i] - 50
+                            lower_right = self.x[i] + 50, self.y[i] + 50
+                            cv2.rectangle(img, upper_left, lower_right, (0, 255, 0), 5)
                             pixel = str(i+1)
-                            text_loc = self.x[i] + 3, self.y[i] - 10
+                            text_loc = self.x[i] + 45, self.y[i] - 55
                             cv2.putText(img, str(self.dis[i]) + " km", text_loc, cv2.FONT_HERSHEY_COMPLEX, 
-                                        0.5, (0, 0, 255))            
+                                        1.5, (255, 0, 0), 3)            
                     except Exception as e:
                         print("error : ", e)
                         pass
                 else:
                     pass
                 h, w, c=  img.shape
-
-                # 하단 컨텐츠 소스
-                # Dark background for the textual contents on the bottom
-                subimg = img.copy()
-                cv2.rectangle(subimg, (0, h - 100), (w, h), (0, 0, 0), -1)
-                img = cv2.addWeighted(img, 0.5, subimg, 0.5, 1)
-
-
                 # GUI에 작업한 이미지 출력
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
-                h, w, c = img.shape
                 qImg = QtGui.QImage(img.data, w, h, w * c, QtGui.QImage.Format_RGB888)
                 pixmap = QtGui.QPixmap.fromImage(qImg)
+                # QPixmap의 크기를 종횡비(KeepAspectRatio)를 유지하고 이중 선형 필터링을(SmoothTransformation) 사용하여 변환된다. 
+                pixmap = pixmap.scaled(self.width(), self.height(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
                 self.label.setPixmap(pixmap)
                 cv2.waitKey(0)                
             else:                
@@ -123,7 +118,7 @@ class pyqt_ipcam(QWidget):
 
         self.cap.release()
 
-        # 타겟 정보 저장.
+        # 종료될 때 타겟 정보를 실행된 카메라에 맞춰서 저장
         if len(self.x) >= 0:
             for i in range(len(self.x)):
                 print(self.x[i], ", ", self.y[i], ", ", self.dis[i])
@@ -137,7 +132,6 @@ class pyqt_ipcam(QWidget):
         
         print("Thread end.")
 
-
     # 임의 타겟들을 정함 get_target    
     def get_target(self):
         self.x = []
@@ -149,6 +143,12 @@ class pyqt_ipcam(QWidget):
             self.y = result.y.tolist()
             self.dis = result.dis.tolist()
             print("target을 불러옵니다.")
+
+    # QWidget Layout의 사이즈가 변환될 때 호출되는 메서드
+    # label 위젯의 크기를 변환하기 위해 재정의한다.
+    def resizeEvent(self, event):
+        print(self.width(), ", ", self.height())
+        self.label.resize(self.width(), self.height())
     
     # 영상을 읽는다
     # Thread로 실행한다.
@@ -173,7 +173,8 @@ class pyqt_ipcam(QWidget):
         print("exit")
         self.stop()
         self.close()
-
+        
+    # 키보드 입력 이벤트
     def keyPressEvent(self, e):
         if e.key() == Qt.Key_T:
             self.start()
@@ -182,14 +183,14 @@ class pyqt_ipcam(QWidget):
         elif e.key() == Qt.Key_Q:
             self.onExit()
         elif e.key() == Qt.Key_F:
-            # self.showMaximized()
-            self.showFullScreen()
+            self.showMaximized()
+            # self.showFullScreen()
         elif e.key() == Qt.Key_N:
             self.showNormal()
         elif e.key() == Qt.Key_1:
             self.stop()
             self.camname = "hanhwa"
-            self.ADD = "rtsp://nalgaem:zf!w58DN4jCu@192.168.100.115/profile2/media.smp"
+            self.ADD = "rtsp://admin:sijung5520@192.168.100.121/profile2/media.smp"
             self.start()
         elif e.key() == Qt.Key_2:
             self.stop()
@@ -200,17 +201,22 @@ class pyqt_ipcam(QWidget):
             self.stop()
             self.camname = "img"            
             self.start()
+        elif e.key() == Qt.Key_4:
+            self.stop()
+            self.camname = "hanhwa"
+            self.ADD = "rtsp://admin:G85^mdPzCXr2@192.168.100.115/profile2/media.smp"
+            self.start()
         
-
     # 마우스 왼쪽 버튼을 누르면 타겟을 추가, 오른쪽 버튼은 최근에 추가된 타겟을 제거.
     def mousePressEvent(self, e):        
         if e.button() == Qt.LeftButton:
             text, ok = QInputDialog.getText(self, '타겟거리입력', '거리(km)')
             if ok:
                 self.dis.append(str(text))
-                self.x.append(e.x())
-                self.y.append(e.y())
-                print("타겟위치:", e.x(),", ", e.y())
+                # Label의 크기와 카메라 원본 이미지 해상도의 차이를 고려해 계산한다. 약 3.175배
+                self.x.append(int(e.x() * 3.175))
+                self.y.append(int(e.y() * 3.175))
+                print("타겟위치:", int(e.x() * 3.175),", ", int(e.y() * 3.175))
                 time.sleep(1)
         elif e.button() == Qt.RightButton:
             if len(self.x) >= 1:
