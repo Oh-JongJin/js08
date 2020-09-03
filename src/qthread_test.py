8366cc46b3d844a006dc608b9d1d6160072c837b
Chae Seong Min <39476614+chaezz@users.noreply.github.com>
2020-09-03 11:22:45 +0900

    Update qthread_test.py
    '타겟'을 '영상 목표'로 수정했습니다.

--- src/qthread_test.py
+++ src/qthread_test.py
@@ -38,7 +38,7 @@ class worker(QThread):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.camname = ""
-    # 저장된 타겟들을 불러온다 없으면 빈 리스트만 선언
+    # 저장된 영상 목표들을 불러온다 없으면 빈 리스트만 선언
     def get_target(self):
         self.x = []
         self.y = []
@@ -53,7 +53,7 @@ class worker(QThread):
     
     def run(self):
         global img
-        # 타겟들을 불러오기  
+        # 영상 목표들을 불러오기  
         self.get_target()
         print(self.camname)
         print(self.ADD)
@@ -97,7 +97,7 @@ class worker(QThread):
 
             # 이미지를 읽어오면 실행
             if ret:
-                # 타겟들을 사각형으로 호출 get_target
+                # 영상 목표들을 사각형으로 호출 get_target
                 if len(self.x) >= 0:
                     try:
                         for i in range(len(self.x)):
@@ -127,7 +127,7 @@ class worker(QThread):
                 print("Cannot read frame.")
                 break
             self.cap.release()
-            # 타겟 정보 저장.
+            # 영상 목표 정보 저장.
 
         if len(self.x) >= 0:
             for i in range(len(self.x)):
@@ -219,26 +219,26 @@ class pyqt_ipcam(QWidget):
             self.start()
         
 
-    # 마우스 왼쪽 버튼을 누르면 타겟을 추가, 오른쪽 버튼은 최근에 추가된 타겟을 제거.
+    # 마우스 왼쪽 버튼을 누르면 영상 목표를 추가, 오른쪽 버튼은 최근에 추가된 영상 목표를 제거.
     def mousePressEvent(self, e):        
         if e.button() == Qt.LeftButton:
-            text, ok = QInputDialog.getText(self, '타겟거리입력', '거리(km)')
+            text, ok = QInputDialog.getText(self, '영상 목표 거리입력', '거리(km)')
             if ok:
                 self.dis.append(str(text))
                 self.x.append(e.x())
                 self.y.append(e.y())
-                print("타겟위치:", e.x(),", ", e.y())
+                print("영상 목표 위치:", e.x(),", ", e.y())
                 time.sleep(1)
         elif e.button() == Qt.RightButton:
             if len(self.x) >= 1:
                 del self.x[-1]
                 del self.y[-1]
                 del self.dis[-1]            
-                print("타겟을 제거했습니다.")       
+                print("영상 목표를 제거했습니다.")       
                 time.sleep(1)
             else:
                 pass
-                print("제거할 타겟이 없습니다.")
+                print("제거할 영상 목표가 없습니다.")
     def mouseReleaseEvent(self, e):
         return super().mouseReleaseEvent(e)
     
