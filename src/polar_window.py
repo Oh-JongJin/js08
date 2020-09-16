cb6435adb8bb6b4823d58a6c01e3beb7c1810ef3
Kyungwon Chun <ruddyscent@gmail.com>
2020-09-16 14:15:49 +0900

    Update target plot every 0.1s

--- src/polar_window.py
+++ src/polar_window.py
@@ -31,6 +31,33 @@ class TargetPlotWindow(Ui_TargetPlot):
         super().setupUi(TargetPlot)
 
         pi = np.pi
+        
+        self.canvas = MplCanvas(TargetPlot, width=5, height=4, dpi=100)
+        self.update_plot()
+        
+        # plot.show()
+        # rlab = plot.axes.ylabel("(mi)")
+        # rlab.set_position((2, 0.2))
+        # rlab.set_rotation(45)
+
+        layout = QtWidgets.QVBoxLayout() 
+        layout.addWidget(self.canvas)
+        TargetPlot.setLayout(layout)
+
+        # self.canvas = FigureCanvasQTAgg(fig)
+        # self.canvas.draw_idle()
+        # self.lay = QtWidgets.QHBoxLayout()
+        # self.setLayout(self.lay)
+        # self.lay.addWidget(self.canvas)
+        # self.show()
+
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(100)
+        self.timer.timeout.connect(self.update_plot)
+        self.timer.start()
+
+    def update_plot(self):
+        pi = np.pi
 
         csv = pd.read_csv('target/PNM-9030V.csv', names=['target_name', 'target_x', 'target_y', 'distance', 'predict'])
 
@@ -42,34 +69,23 @@ class TargetPlotWindow(Ui_TargetPlot):
         dist_list = dist.values.tolist()
         dist_list.pop(0)
 
-        plot = MplCanvas(TargetPlot, width=5, height=4, dpi=100)
-        # fig = plt.figure("Hello")
-        plot.axes.set_facecolor('skyblue')
-        plot.axes.set_thetamin(-90)
-        plot.axes.set_thetamax(90)
-
+        self.canvas.axes.cla()  # Clear the canvas.
+        self.canvas.axes.set_facecolor('skyblue')
+        self.canvas.axes.set_thetamin(-90)
+        self.canvas.axes.set_thetamax(90)
         for i in range(len(dist_list)):
             angle_list[i] = float((float(angle_list[i]) * 1920) / 6096)
             angle_list[i] = round((float(angle_list[i]) * 180) / 1920, 3)
-            plot.axes.scatter((pi * angle_list[i] / 180) + (-pi / 2), float(dist_list[i]), s=20, cmap='hsv', alpha=0.75)
-            plot.axes.text((pi * angle_list[i] / 180) + (-pi / 2), float(dist_list[i]), str(i+1))
+            self.canvas.axes.scatter((pi * angle_list[i] / 180) + (-pi / 2), float(dist_list[i]), s=20, cmap='hsv', alpha=0.75)
+            self.canvas.axes.text((pi * angle_list[i] / 180) + (-pi / 2), float(dist_list[i]), str(i+1))
 
-        plot.axes.set_xticks([-pi / 2, -pi / 6, -pi / 3, 0, pi / 6, pi / 3, pi / 2])
-        plot.axes.set_theta_zero_location("N")
-        plot.axes.set_theta_direction(-1)
+        # Drop off the first y element, append a new one.
+        self.canvas.axes.scatter((pi * angle_list[i] / 180) + (-pi / 2), float(dist_list[i]), s=20, cmap='hsv', alpha=0.75)
+        self.canvas.axes.text((pi * angle_list[i] / 180) + (-pi / 2), float(dist_list[i]), str(i+1))
 
-        # plot.show()
-        # rlab = plot.axes.ylabel("(mi)")
-        # rlab.set_position((2, 0.2))
-        # rlab.set_rotation(45)
-
-        layout = QtWidgets.QVBoxLayout() 
-        layout.addWidget(plot)
-        TargetPlot.setLayout(layout)
-
-        # self.canvas = FigureCanvasQTAgg(fig)
-        # self.canvas.draw_idle()
-        # self.lay = QtWidgets.QHBoxLayout()
-        # self.setLayout(self.lay)
-        # self.lay.addWidget(self.canvas)
-        # self.show()
+        self.canvas.axes.set_xticks([-pi / 2, -pi / 6, -pi / 3, 0, pi / 6, pi / 3, pi / 2])
+        self.canvas.axes.set_theta_zero_location("N")
+        self.canvas.axes.set_theta_direction(-1)
+        
+        # Trigger the canvas to update and redraw.
+        self.canvas.draw()
