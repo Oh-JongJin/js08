cd61a09bce45f2a681e763177d42faac44b1b7e2
Oh-JongJin <5jx2oh@gmail.com>
2021-06-10 17:33:26 +0900

    Edit install influx file

--- src/influx_install.sh
+++ src/influx_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "User name:" "$USERNAME"
+echo "influxdb installing..."
 wget https://dl.influxdata.com/influxdb/releases/influxdb_1.7.8_amd64.deb
 sudo dpkg -i influxdb_1.7.8_amd64.deb
 
