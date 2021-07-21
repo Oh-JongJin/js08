9045f9ebfae79bbe6b39a58a3bbc96aeeb1fe185
Oh-JongJin <5jx2oh@gmail.com>
2021-07-21 15:22:17 +0900

    Add frame capture function

--- src/video_widget_2.py
+++ src/video_widget_2.py
@@ -5,6 +5,12 @@
 #     ruddyscent@gmail.com (Kyungwon Chun)
 #     5jx2oh@gmail.com (Jongjin Oh)
 
+import time
+import os
+import cv2
+import numpy as np
+from video_thread import VideoThread
+
 from PyQt5.QtCore import Qt, QUrl, pyqtSlot
 from PyQt5.QtWidgets import QWidget, QGraphicsScene, QGraphicsView, QVBoxLayout
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
@@ -14,34 +20,31 @@ VIDEO_SRC1 = "rtsp://admin:sijung5520@d617.asuscomm.com:1554/profile2/media.smp"
 VIDEO_SRC2 = "rtsp://admin:sijung5520@d617.asuscomm.com:2554/profile2/media.smp"
 VIDEO_SRC3 = "rtsp://admin:sijung5520@d617.asuscomm.com:3554/profile2/media.smp"
 
+
 class Js06VideoWidget2(QWidget):
     def __init__(self, parent=None):
         super(Js06VideoWidget2, self).__init__(parent)
 
-        # self.blank_lbl = QLabel()
         self.scene = QGraphicsScene(self)
         self.graphicView = QGraphicsView(self.scene)
         self.graphicView.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.graphicView.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.video_item = QGraphicsVideoItem()
         self.scene.addItem(self.video_item)
-        # self.rect_item = QGraphicsRectItem(QRectF(50, 50, 40, 40), self.video_item)
-        # self.rect_item.setBrush(QBrush(Qt.green))
-        # self.rect_item.setPen(QPen(Qt.red))
 
         self._player = QMediaPlayer(self, QMediaPlayer.VideoSurface)
-        # self._player.stateChanged.connect(self.onCameraChange)
         self._player.setVideoOutput(self.video_item)
-        # self._player.setMedia(QMediaContent(QUrl(VIDEO_SRC3)))
         self._player.setPosition(0)
 
         layout = QVBoxLayout(self)
         layout.addWidget(self.graphicView)
+        self.camera_name = None
 
-        # self.qtimer = QTimer()
-        # self.qtimer.setInterval(2000)
-        # self.qtimer.timeout.connect(self.inference)
-        # self.qtimer.start()
+        self.filepath = os.path.join(os.getcwd(), "target")
+        try:
+            os.makedirs(self.filepath, exist_ok=True)
+        except OSError:
+            pass
     # end of __init__
 
     @pyqtSlot(QMediaPlayer.State)
@@ -54,14 +57,33 @@ class Js06VideoWidget2(QWidget):
     def onCameraChange(self, url):
         self._player.setMedia(QMediaContent(QUrl(url)))
         self._player.play()
-        # self.graphicView.fitInView(self.video_item)
+
+        self.video_thread = VideoThread(url)
+        self.video_thread.update_pixmap_signal.connect(self.convert_cv_qt)
+        self.video_thread.start()
     # end of onCameraChange
 
-    def inference(self):
-        self.graphicView.fitInView(self.video_item)
-    # end fo inference
+    def convert_cv_qt(self, cv_img):
+        rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+        self.epoch = time.strftime("%Y%m%d%H%M%S", time.localtime(time.time()))
+        if self.epoch[-2:] == "00":
+            self.save_frame(cv_img, self.epoch)
+    # end of conver_cv_qt
+
+    def save_frame(self, image: np.ndarray, epoch: str):
+        image_path = os.path.join(self.filepath, "image", f"{self.camera_name}", f"{epoch[2:6]}")
+        file_name = f"{epoch}"
+        if not os.path.isdir(image_path):
+            os.makedirs(image_path)
+        if not os.path.isfile(f"{image_path}/{file_name}.jpg"):
+            cv2.imwrite(f"{image_path}/{file_name}.jpg", image)
+        del image
+        del image_path
+        cv2.destroyAllWindows()
+    # end of save_frame
 
 # end of VideoWidget2
+
 
 if __name__ == '__main__':
     import sys
