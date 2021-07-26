e6752db353667b39106570d3679b52012a74d9ef
Oh-JongJin <5jx2oh@gmail.com>
2021-07-26 16:00:18 +0900

    Add random value in Polar plot

--- src/target_plot_widget_2.py
+++ src/target_plot_widget_2.py
@@ -17,7 +17,6 @@ class Js06TargetPlotWidget(QWidget):
         super(Js06TargetPlotWidget, self).__init__(parent)
         layout = QVBoxLayout(self)
 
-
         pi = np.pi
         self._plot_ref_red = None
         self._plot_ref_green = None
@@ -38,11 +37,43 @@ class Js06TargetPlotWidget(QWidget):
         self.ylabel = self.axes.set_ylabel("(km)", fontsize=7)
         self.ylabel.set_position((2, 0.2))
         self.ylabel.set_rotation(45)
-        vis = int(np.random.randint(0, 10000, size=1))
-        self.xlabel = self.axes.set_xlabel(f"Visibility: {vis} m", fontsize=20)
+        self.vis = int(np.random.randint(0, 10000, size=1))
+        self.xlabel = self.axes.set_xlabel(f"Visibility: {self.vis} m", fontsize=20)
         plt.rcParams.update({'font.size': 7})
 
         layout.addWidget(self.canvas)
+
+        self._timer = self.canvas.new_timer(1000)
+        self._timer.add_callback(self._update_canvas)
+        self._timer.start()
+
+    def _plot_canvas(self):
+        pi = np.pi
+
+        self.axes.set_thetamin(-90)
+        self.axes.set_thetamax(90)
+        self.axes.set_xticks([-pi / 2, -pi / 6, -pi / 3, 0, pi / 6, pi / 3, pi / 2])
+        self.axes.set_theta_zero_location("N")
+        self.axes.set_theta_direction(-1)
+        self.ylabel = self.axes.set_ylabel("(km)", fontsize=7)
+        self.ylabel.set_position((2, 0.2))
+        self.ylabel.set_rotation(45)
+        self.xlabel = self.axes.set_xlabel(f"Visibility: {self.vis} m", fontsize=20)
+        plt.rcParams.update({'font.size': 7})
+
+    def _update_canvas(self):
+        self.axes.clear()
+        self._plot_canvas()
+
+        prime_x_left = -(np.random.rand(24))
+        prime_x_right = -prime_x_left
+        prime_x = np.concatenate((prime_x_left, prime_x_right))
+        plot_x = np.array(prime_x) * np.pi / 2
+        distance = np.random.randint(0, 20000, size=48)
+        self.vis = max(distance)
+        for i, xy in enumerate(zip(plot_x, distance), start=0):
+            _plot_red_green, = self.axes.plot(plot_x[i], distance[i] / 1000, "go")
+        self.canvas.draw()
 
 
 if __name__ == '__main__':
