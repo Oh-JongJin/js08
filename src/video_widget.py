83ca61de11f008a6a158b0f55c48caaa026372d2
Kyungwon Chun <ruddyscent@gmail.com>
2021-07-30 11:10:06 +0900

    Fix to play video stream

--- src/video_widget.py
+++ src/video_widget.py
@@ -13,34 +13,38 @@ from PyQt5.QtMultimediaWidgets import QVideoWidget
 VIDEO_SRC1 = "rtsp://admin:sijung5520@d617.asuscomm.com:1554/profile2/media.smp"
 VIDEO_SRC2 = "rtsp://admin:sijung5520@d617.asuscomm.com:2554/profile2/media.smp"
 VIDEO_SRC3 = "rtsp://admin:sijung5520@d617.asuscomm.com:3554/profile2/media.smp"
+VIDEO_SRC4 = "rtsp://admin:sijung5520@d617.asuscomm.com:4554/profile2/media.smp"
+VIDEO_SRC5 = "rtsp://admin:sijung5520@d617.asuscomm.com:5554/profile2/media.smp"
 
 class Js06VideoWidget(QWidget):
+    """Video stream player using QVideoWidget
+    """
     def __init__(self, parent=None):
         super(Js06VideoWidget, self).__init__(parent)
-        self._viewer = QVideoWidget()  # self is required?
-        self._player = QMediaPlayer(self, QMediaPlayer.VideoSurface)
-        self._player.setVideoOutput(self._viewer)
+        self.viewer = QVideoWidget()  # self is required?
+        self.player = QMediaPlayer(self, QMediaPlayer.VideoSurface)
+        self.player.setVideoOutput(self.viewer)
         # self._player.setMedia(QMediaContent(QUrl(VIDEO_SRC3)))
-        self._player.setPosition(0)  # Required?
-        self._viewer.show()
+        self.player.setPosition(0)  # Required?
+        self.viewer.show()
         # self._player.play()
         layout = QVBoxLayout(self)
-        layout.addWidget(self._viewer)
-        self._viewer.setGeometry(0, 0, 100, 100)
+        layout.addWidget(self.viewer)
+        self.viewer.setGeometry(0, 0, 100, 100)
 
-        self._viewer.mousePressEvent = self.viewer_mousePressEvent
+        self.viewer.mousePressEvent = self.viewer_mousePressEvent
     # end of __init__
 
     @pyqtSlot(str)
     def onCameraChange(self, url):
-        self._player.setMedia(QMediaContent(QUrl(url)))
-        self._player.play()
+        self.player.setMedia(QMediaContent(QUrl(url)))
+        self.player.play()
     # end of onCameraChange
 
     def viewer_mousePressEvent(self, event):
         print(event.pos())
-        print(f"videoWidget: {self._viewer.sizeHint()}")
-        # self.resize(self._viewer.sizeHint().width(), self._viewer.sizeHint().height())
+        print(f"videoWidget: {self.viewer.sizeHint()}")
+        # self.resize(self.viewer.sizeHint().width(), self.viewer.sizeHint().height())
         # self.resize(300, 300)
     # end of viewer_mousePressEvent
 
@@ -52,6 +56,8 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     window = Js06VideoWidget()
+    window.player.setMedia(QMediaContent(QUrl(VIDEO_SRC5)))
+    window.player.play()
     window.show()
     sys.exit(app.exec_())
 
