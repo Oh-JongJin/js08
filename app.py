0786ff42f412f77577e029633744ff540a4bb285
Kyungwon Chun <ruddyscent@gmail.com>
2021-08-09 23:30:27 +0900

    Gather MVC codes into three scripts for the sake of simplicity.

--- app.py
+++ app.py
@@ -1,43 +1,38 @@
 #!/usr/bin/env python3
- 
+#
+# Copyright 2020-2021 Sijung Co., Ltd.
+# 
+# Authors: 
+#     ruddyscent@gmail.com (Kyungwon Chun)
+#     5jx2oh@gmail.com (Jongjin Oh)
+
 import sys
 
-from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtGui import QIcon # pylint: disable=no-name-in-module
-from PyQt5.QtWidgets import QApplication, QMessageBox # pylint: disable=no-name-in-module
+from PyQt5.QtWidgets import QApplication # pylint: disable=no-name-in-module
 
-from views.main_view import Js06MainWindow
-from model.settings import Js06Settings
-from model.model import Js06Attribute
+from js06.view import Js06MainView
+from js06.model import Js06Model
+from js06.controller import Js06MainCtrl
+from js06 import js06_rc
 
-import js06_rc
-
-class Js06(QApplication):
-    abnormal_shutdown = pyqtSignal()
-
-    def __init__(self, sys_argv):
-        super().__init__(sys_argv)
-
-        self.model = Js06Attribute()
-        self.main_ctrl = Js06MainCtrl()
-        self.main_view = Js06MainWindow()
-        self.abnormal_shutdown.connect(self.main_view.restore_default)
-
-        shutdown_status = Js06Settings.get('normal_shutdown')
-        if not shutdown_status:
-            self.abnormal_shutdown.emit()
-
-        app_icon = QIcon(":icon/logo.png")
-        self.main_view.setWindowIcon(app_icon)
-
-        self.main_view.show()
-    # end of __init__
-
-# end of Js06
-
-if __name__ == '__main__':
-
-    app = Js06(sys.argv)
+def main():
+    """Main function"""
+    # Create an instance of `QApplication`
+    app = QApplication(sys.argv)
+    # Show the calculator's GUI
+    view = Js06MainView()
+    view.show()
+    # Create instances of the model and the controller
+    model = Js06Model()
+    Js06MainCtrl(model=model, view=view)
+    # Set icon of the app
+    app_icon = QIcon(":icon/logo.png")
+    view.setWindowIcon(app_icon)
+    # Execute calculator's main loop
     sys.exit(app.exec_())
-    
+
+if __name__ == "__main__":
+    main()
+
 # end of app.py
---
+++ js06/controller.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+#
+# Copyright 2020-2021 Sijung Co., Ltd.
+# 
+# Authors: 
+#     ruddyscent@gmail.com (Kyungwon Chun)
+#     5jx2oh@gmail.com (Jongjin Oh)
+
+from pymongo import MongoClient
+
+from PyQt5.QtCore import QObject, QThreadPool, pyqtSignal, pyqtSlot # pylint: disable=no-name-in-module
+
+from js06.model import Js06Model, Js06Settings
+from js06.view import Js06MainView
+
+class Js06MainCtrl(QObject):
+    abnormal_shutdown = pyqtSignal()
+
+    def __init__(self, model:Js06Model, view:Js06MainView):
+        super().__init__()
+
+        self.thread_pool = QThreadPool.globalInstance()
+
+        self._model = model
+        self._view = view
+
+        # Connect signals and slots
+        self._view.restore_defaults_requested.connect(self.restore_defaults)
+        self.abnormal_shutdown.connect(self._view.ask_restore_default)
+
+        self._view.main_view_closed.connect(self.close_process)
+
+        self.init()
+    # end of __init__
+
+    def init(self):
+        self.check_exit_status()
+
+        db_host = Js06Settings.get('db_host')
+        db_port = Js06Settings.get('db_port')
+        db_name = Js06Settings.get('db_name')
+        self._model.connect_to_db(db_host, db_port, db_name)
+
+        attr = self._model.read_attr()
+        print(attr.to_dict())
+    # end of init
+    
+    def check_exit_status(self):
+        normal_exit = Js06Settings.get('normal_shutdown')
+        if not normal_exit:
+            self.abnormal_shutdown.emit()
+        Js06Settings.set('normal_shutdown', False)
+    # end of check_exit_stauts
+
+    @pyqtSlot()
+    def close_process(self):
+        print("DEBUG: inside close_process method")
+        Js06Settings.set('normal_shutdown', True)
+    # end of close_process
+
+    def get_attr(self, model):
+        if self.attr.count_documents({}):
+            attr_doc = list(self.attr.find().sort("_id", -1).limit(1))[0]
+        else:
+            attr_doc = model.to_dict()
+        return attr_doc
+    # end of get_attr
+    
+    def set_attr(self, model):
+        self.attr.insert_one(model.to_dict())
+    # end of set_attr
+
+    @pyqtSlot(int)
+    def set_current_camera(self, num:int):
+        Js06Settings.set('camera', num)
+    # end of set_curent_camera
+
+    @pyqtSlot()
+    def restore_defaults(self):
+        print("DEBUG: inside restore_defaults")
+        Js06Settings.restore_defaults()
+
+    # @pyqtSlot(bool)
+    # def set_normal_shutdown(self):
+    #      Js06Settings.set('normal_shutdown', True)
+
+# end of Js06MainCtrl
+
+# if __name__ == '__main__':
+#     ctrl = Js06MainCtrl()
+#     print(ctrl.get_attr)
+
+# end of main_ctrl.py
+ 
